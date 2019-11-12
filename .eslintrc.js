020dcbb252cf9f72e6ce2455cd818cf1bbdbd3fa
Gapur Kassym <gapur.kassym@gmail.com>
2019-11-12 15:35:00 +0600

    feat(app): first init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: "airbnb-base",
+  env: {
+    "node": true,
+    "mocha": true
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    'func-names': "off",
+    "object-curly-newline": ['error', {
+      minProperties: 10, multiline: true, consistent: true,
+    }],
+    "camelcase": "off",
+    "no-param-reassign": "off",
+    "no-plusplus": "off"
+  }
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+const sorts = require('./sorts');
+
+sorts();
