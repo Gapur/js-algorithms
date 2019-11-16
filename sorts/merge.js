b97bb6488be575c4388d722fe0c2a9090528929f
Gapur Kassym <gapur.kassym@gmail.com>
2019-11-16 21:55:55 +0600

    refactor(sort): merge sort

--- sorts/merge.js
+++ sorts/merge.js
@@ -1,30 +1,51 @@
-const merge = (left, right) => {
-  const result = [];
-  const leftLen = left.length;
-  const rightLen = right.length;
-  let l = 0;
-  let r = 0;
-  while (l < leftLen && r < rightLen) {
-    if (left[l] < right[r]) {
-      result.push(left[l]);
-      l++;
+const mergeSortedArrays = (leftArray, rightArray) => {
+  let sortedArray = [];
+
+  // In case if arrays are not of size 1.
+  while (leftArray.length && rightArray.length) {
+    let minimumElement = null;
+
+    // Find minimum element of two arrays.
+    if (leftArray[0] <= rightArray[0]) {
+      minimumElement = leftArray.shift();
     } else {
-      result.push(right[r]);
-      r++;
+      minimumElement = rightArray.shift();
     }
+
+    // Push the minimum element of two arrays to the sorted array.
+    sortedArray.push(minimumElement);
   }
-  return result.concat(left.slice(l)).concat(right.slice(r));
+
+  // If one of two array still have elements we need to just concatenate
+  // this element to the sorted array since it is already sorted.
+  if (leftArray.length) {
+    sortedArray = sortedArray.concat(leftArray);
+  }
+
+  if (rightArray.length) {
+    sortedArray = sortedArray.concat(rightArray);
+  }
+
+  return sortedArray;
 };
 
-const sort = (arr) => {
-  const len = arr.length;
-  if (len < 2) {
-    return arr;
+const sort = (originalArray) => {
+  // If array is empty or consists of one element then return this array since it is sorted.
+  if (originalArray.length <= 1) {
+    return originalArray;
   }
-  const mid = Math.floor(len / 2);
-  const left = arr.slice(0, mid);
-  const right = arr.slice(mid);
-  return merge(sort(left), sort(right));
+
+  // Split array on two halves.
+  const middleIndex = Math.floor(originalArray.length / 2);
+  const leftArray = originalArray.slice(0, middleIndex);
+  const rightArray = originalArray.slice(middleIndex, originalArray.length);
+
+  // Sort two halves of split array
+  const leftSortedArray = sort(leftArray);
+  const rightSortedArray = sort(rightArray);
+
+  // Merge two sorted arrays into one.
+  return mergeSortedArrays(leftSortedArray, rightSortedArray);
 };
 
 module.exports = sort;
