407e1936cb4c1a1e34966def5d132f061725118b
Gapur Kassym <gapur.kassym@gmail.com>
2019-11-18 18:08:03 +0600

    feat(sets): longest inscreasing subsequence

--- sets/index.js
+++ sets/index.js
@@ -1,4 +1,5 @@
 const longestCommonSubsequence = require('./longestCommonSubsequence');
+const longestIncreasingSubsequence = require('./longestIncreasingSubsequence');
 
 console.log('start sets -------');
 
@@ -8,6 +9,12 @@ const set2 = 'AMDFF';
 const lcs = longestCommonSubsequence(set1, set2);
 
 console.log(`Longest Common Subsequence: set1=${set1} set2=${set2} ${lcs}`);
+
+const sequence = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15];
+
+const lis = longestIncreasingSubsequence(sequence);
+
+console.log(`Longest Increasing Subsequence: ${lis} sequence=${sequence.join(', ')} `)
 
 console.log('end sets --------');
 
