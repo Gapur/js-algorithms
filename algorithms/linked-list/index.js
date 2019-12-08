e26aa449819eeea130898860ca88d86639833f6c
Gapur Kassym <gapur.kassym@gmail.com>
2019-12-08 23:05:27 +0600

    feat(traversal): impelement traversal

--- algorithms/linked-list/index.js
+++ algorithms/linked-list/index.js
@@ -1,5 +1,6 @@
 const LinkedList = require('../../data-structures/linked-list/LinkedList');
 const reverseTraversal = require('./reverseTraversal');
+const traversal = require('./traversal');
 
 console.log('start linked-list -------');
 
@@ -19,6 +20,24 @@ console.log(`LinkedList ${linkedList.toString()}`);
 reverseTraversal(linkedList, traversalCallback);
 
 console.log(`Reverse Traversal LinkedList ${traversedNodeValues.toString()}`);
+
+const linkedList1 = new LinkedList();
+
+linkedList1
+  .append(1)
+  .append(2)
+  .append(3);
+
+const traversedNodeValues1 = [];
+const traversalCallback1 = (nodeValue) => {
+  traversedNodeValues1.push(nodeValue);
+};
+
+console.log(`LinkedList1 ${linkedList1.toString()}`);
+
+traversal(linkedList1, traversalCallback1);
+
+console.log(`Traversal LinkedList ${traversedNodeValues1.toString()}`);
 
 console.log('end linked-list --------');
 
