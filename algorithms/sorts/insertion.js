427d79f1aa929481cd9dd11931b825f0ca4406ca
Gapur Kassym <gapur.kassym@gmail.com>
2020-01-23 20:58:39 +0600

    refactor(sort): change insertion sort

--- algorithms/sorts/insertion.js
+++ algorithms/sorts/insertion.js
@@ -1,14 +1,18 @@
-const sort = (array) => {
-  for (let i = 1; i < array.length; i++) {
-    const key = array[i];
-    let j = i - 1;
-    while (j >= 0 && array[j] > key) {
-      array[j + 1] = array[j];
-      j -= 1;
+const InsertionSort = (array) => {
+  for (let i = 0; i < array.length; i += 1) {
+    let currentIndex = i;
+    while (
+      array[currentIndex - 1] !== undefined
+        && array[currentIndex] < array[currentIndex - 1]
+    ) {
+      const tmp = array[currentIndex - 1];
+      array[currentIndex - 1] = array[currentIndex];
+      array[currentIndex] = tmp;
+      currentIndex -= 1;
     }
-    array[j + 1] = key;
   }
+
   return array;
 };
 
-module.exports = sort;
+module.exports = InsertionSort;
