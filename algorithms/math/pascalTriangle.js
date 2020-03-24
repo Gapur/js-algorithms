961ed91da31c1566648af98677f0183f772fe20a
Gapur Kassym <gapur.kassym@gmail.com>
2020-03-25 00:56:33 +0600

    pascalTriangle

---
+++ algorithms/math/pascalTriangle.js
@@ -0,0 +1,14 @@
+function pascalTriangle(lineNumber) {
+  const currentLine = [1];
+
+  const currentLineSize = lineNumber + 1;
+
+  for (let numIndex = 1; numIndex < currentLineSize; numIndex += 1) {
+    // See explanation of this formula in README.
+    currentLine[numIndex] = currentLine[numIndex - 1] * (lineNumber - numIndex + 1) / numIndex;
+  }
+
+  return currentLine;
+}
+
+module.exports = pascalTriangle;
